a33447b593a14aebcc8527733624320fe8469ea9
richie <richie.kwon1@gmail.com>
2022-05-04 20:43:00 +0100

    annotation in the data model

--- NetCore.Data/DataModels/User.cs
+++ NetCore.Data/DataModels/User.cs
@@ -1,13 +1,27 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace NetCore.Data.DataModels
 {
     public class User
     {
+        [Key, StringLength(50), Column(TypeName = "varchar(50)")]
         public string UserId { get; set; }
+        
+        [Required, StringLength(100), Column(TypeName = "nvarchar(100)")]
         public string UserName { get; set; }
+        
+        [Required, StringLength(320), Column(TypeName = "varchar(320)")]
         public string UserEmail { get; set; }
+        
+        [Required, StringLength(130), Column(TypeName = "nvarchar(130)")]
         public string Password { get; set; }
+
+        [Required]
+        public bool IsMembershipWithdrawn { get; set; }
+
+        [Required]
         public DateTime JoinedUtcDate { get; set; }
     }
 }
